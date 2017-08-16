a37a3b485ff1e18e814c5212f30ec4ef019fe378
Charlie Hield <charlie.hield@collemcvoy.com>
2017-08-16 11:02:35 -0500

    Add Netlify prerender meta tag

--- poi.config.js
+++ poi.config.js
@@ -4,10 +4,14 @@ const PrerenderSpaPlugin = require('prerender-spa-plugin')
 // const usersRoutes = [...Array(10).keys()].map(i => `/users/${++i}`)
 
 module.exports = {
+  html: {
+    title: 'Vue Prerender SPA Demo',
+    template: path.join(__dirname, '/src/layout.ejs')
+  },
   // webpack(config) {
   //   const plugin = new PrerenderSpaPlugin(
   //     // entry
-  //     path.join(`${__dirname}/dist`),
+  //     path.join(__dirname, '/dist'),
   //     // routes
   //     ['/', '/about', ...usersRoutes],
   //     // options
